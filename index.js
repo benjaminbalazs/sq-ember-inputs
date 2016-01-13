58fbd0002bee70f187ec15b46c387a2402a318c6
Benjamin Balazs <benjaminbalazs@gmail.com>
2016-01-13 15:22:38 +0400

    fx asset

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'sq-ember-inputs',
   included: function(app) {
     this._super.included(app);
-    app.import('vendor/assets/sq-dropdown-arrow.svg');
+    app.import('vendor/assets/sq-dropdown-arrow.svg', { destDir: 'assets/images' });
     app.import(app.bowerDirectory + '/dragula.js/dist/dragula.min.js');
 	app.import(app.bowerDirectory + '/dragula.js/dist/dragula.min.css');
     app.import(app.bowerDirectory + '/formatter.js/dist/jquery.formatter.min.js');
