b95f6de0f1e63812f47dc60d8b0f0a797deb32d0
Benjamin Balazs <benjaminbalazs@gmail.com>
2016-06-18 17:56:18 +0400

    ct

--- addon/mixins/istouch.js
+++ addon/mixins/istouch.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
 
 	isTouch() {
-
+		
 		var el = document.createElement('div');
    		el.setAttribute('ongesturestart', 'return;'); // or try "ontouchstart"
    		return typeof el.ongesturestart === "function";
