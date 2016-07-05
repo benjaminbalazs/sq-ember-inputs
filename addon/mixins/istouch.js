d130aad7d1c425ca0db45d710db6c54ea5e18495
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2016-07-06 01:08:04 +0400

    touch fx

--- addon/mixins/istouch.js
+++ addon/mixins/istouch.js
@@ -3,10 +3,8 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
 
 	isTouch() {
-		
-		var el = document.createElement('div');
-   		el.setAttribute('ongesturestart', 'return;'); // or try "ontouchstart"
-   		return typeof el.ongesturestart === "function";
+
+		return ('ontouchstart' in window);
 
     },
 
