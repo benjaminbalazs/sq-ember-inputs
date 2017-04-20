a622f03a0dffad7216e60739be013ebc4cc2bb5b
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-04-20 18:28:19 +0400

    fb

--- addon/mixins/istouch.js
+++ addon/mixins/istouch.js
@@ -2,14 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Mixin.create({
 
+	fastboot: Ember.inject.service(),
+
 	isTouch() {
 
-		//return ('ontouchstart' in window);
-		//return true;
-		if ( Ember.$.browser.mobile === true ) {
-			return true;
+		if ( this.get('fastboot.isFastBoot') !== true ) {
+
+			return Ember.$.browser.mobile;
+
 		} else {
-			return false;
+
+			const userAgent = this.get('userAgent');
+			return ( userAgent.get('browser.isMobile') || userAgent.get('browser.isDesktop') );
+
 		}
 
     },
