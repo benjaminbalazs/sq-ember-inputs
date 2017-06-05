68b9153be62984f81ba02558221cc2444e0b5f8b
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-06-05 23:35:13 +0400

    dropfodn ignor

--- addon/components/sq-input-dropdown-item.js
+++ addon/components/sq-input-dropdown-item.js
@@ -3,12 +3,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
 	tagName: 'li',
-	
+
 	classNames: ['sq-align-left'],
 	classNameBindings: ['selected'],
+	attributeBindings: ['dir'],
 
 	click() {
 		this.sendAction('select', this.get('model'));
+	},
+
+	init() {
+
+		this._super();
+
+		if ( this.get('ignoreDirection') === true ) {
+			this.set('dir', 'ltr');
+		}
+
 	},
 
 });
