a7ccaf01aac2c4004fece08509df5a662a5eaec8
benjaminbalazs@gmail.com <benjaminbalazs@Benjamins-iMac-2.local>
2017-08-04 18:32:46 +0400

    fastboot1

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+var FastbootTransform = require('fastboot-transform');
+
 module.exports = {
-  name: 'sq-ember-inputs',
-  included: function(app) {
-    this._super.included(app);
-    app.import('vendor/assets/sq-dropdown-arrow.svg', { destDir: 'assets/images' });
-    if ( !process.env.EMBER_CLI_FASTBOOT ) {
-        app.import(app.bowerDirectory + '/dragula.js/dist/dragula.min.js');
-	    app.import(app.bowerDirectory + '/dragula.js/dist/dragula.min.css');
-        app.import(app.bowerDirectory + '/formatter.js/dist/jquery.formatter.min.js');
+
+    name: 'sq-ember-inputs',
+
+    included: function(app) {
+
+        this._super.included.apply(this, arguments);
+
+        app.import(this.treePaths.vendor + '/dragula/dist/dragula.min.js');
+        app.import(this.treePaths.vendor + '/dragula/dist/dragula.min.css');
+
+        app.import(this.treePaths.vendor + '/jquery.browser/jquery.browser.min.js');
+
+        app.import(this.treePaths.vendor + '/formatter.js/jquery.formatter.min.js');
+
+        app.import('vendor/assets/sq-dropdown-arrow.svg', { destDir: 'assets/images' });
+
+    },
+
+    treeForVendor: function(vendorTree) {
+
+        var trees = [];
+        if ( vendorTree !== undefined ) { trees.push(vendorTree); }
+
+        //
+
+        var dragulaTree = FastbootTransform(new Funnel(path.join(path.dirname(require.resolve('dragula'))), {
+            destDir: 'dragula',
+            include: ['dist/dragula.min.js', 'dist/dragula.min.css']
+        }));
+
+        trees.push(dragulaTree);
+
+        //
+
+        var jqueryBrowser = FastbootTransform(new Funnel(path.join(path.dirname(require.resolve('jquery.browser'))), {
+            destDir: 'jquery.browser',
+            files: ['jquery.browser.min.js']
+        }));
+
+        trees.push(jqueryBrowser);
+
+        //
+
+        var formatterBrowser = FastbootTransform(new Funnel(path.join(path.dirname(require.resolve('formatter.js')), '..'), {
+            destDir: 'formatter.js',
+            files: ['jquery.formatter.min.js']
+        }));
+
+        trees.push(formatterBrowser);
+
+        //
+
+        return new MergeTrees(trees);
+
+    },
+
+    isDevelopingAddon: function() {
+        return true;
     }
-  },
-  isDevelopingAddon: function() {
-    return true;
-  }
+
 };
