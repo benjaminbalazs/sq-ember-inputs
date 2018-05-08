2ae00816a86ede8b725adf3eef0b905b733efaa9
Benjamin Balazs <benjaminbalazs@gmail.com>
2018-05-08 17:22:18 +0400

    update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,23 +3,11 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-<<<<<<< HEAD
 
-    var app = new EmberApp(defaults, {
+    let app = new EmberAddon(defaults, {
+      // Add options here
     });
 
     return app.toTree();
-=======
-  let app = new EmberAddon(defaults, {
-    // Add options here
-  });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
->>>>>>> 4729b84... message
 
 };
